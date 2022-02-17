2d467ce99968ea9c906c7ae7fed9d55d43de2ce8
Crimdahl <crimdahl@outlook.com>
2022-02-16 23:05:33 -0500

    Reworked the way the spritechecker reformats images to improve remonsterate compatibility:
    First, images have their colors checked and, if necessary, reduced.
    Second, images have excess border space at the top and left of the image cropped.
    Third, images have their border at the bottom and right expanded to fit the next whole 8x8 tile.

--- spritechecker.py
+++ spritechecker.py
@@ -1,6 +1,7 @@
 import os
 import datetime
-from PIL import Image
+from PIL import Image, ImageOps
+from collections import Counter
 
 exe_directory = os.getcwd()
 try:
@@ -8,24 +9,26 @@ try:
 except IOError as e:
     print("Error using log file: " + str(e))
 total_sprites = 0
+total_sprites_unchanged = 0
 total_sprites_tmc = 0
-total_sprites_wasteful = 0
-total_sprites_transparent_top_row = 0
+total_sprites_expanded = 0
+total_sprites_cropped = 0
 
 
 def main():
     global exe_directory
     global log_file
     global total_sprites
+    global total_sprites_unchanged
     global total_sprites_tmc
-    global total_sprites_wasteful
-    global total_sprites_transparent_top_row
+    global total_sprites_expanded
+    global total_sprites_cropped
 
     try:
         # Populate the file name lists. Iterates through directories starting at the
         #   directory containing the exe file. Does not traverse directories past
         #   the depth specified by walk_distance.
-        walk_distance = 4
+        walk_distance = 6
         exe_directory_level = exe_directory.count(os.path.sep)
         log("Looking for and analyzing png files in " + exe_directory + " and " + str(walk_distance) +
             " levels of sub-directories.")
@@ -44,109 +47,119 @@ def main():
                 del dirs[:]
             else:
                 for file_name in files:
-                    if file_name.endswith(".png"):
+                    if file_name.lower().endswith(".png"):
                         total_sprites = total_sprites + 1
                         check_image(Image.open(os.path.join(current_walking_directory, file_name)),
                                     current_walking_directory)
         log("Total sprites: " + str(total_sprites))
-        log("Sprites with correct colors: " + str(total_sprites - total_sprites_tmc - total_sprites_wasteful))
-        log("Sprites with too many colors: " + str(total_sprites_tmc))
-        log("Sprites with wasteful palettes: " + str(total_sprites_wasteful))
-        log("Sprites with transparent border row or column: " + str(total_sprites_transparent_top_row))
+        log("Total sprites unchanged: " + str(total_sprites_unchanged))
+        log("Sprites that had too many colors and were converted: " + str(total_sprites_tmc))
+        log("Sprites that had excess top and/or left borders and were cropped: " + str(total_sprites_cropped))
+        log("Sprites that had the bottom and/or right border expanded: " + str(total_sprites_expanded))
     except Exception as e2:
         log("Exception encountered: " + str(e2))
     finally:
         log_file.close()
 
 
+def get_transparency(image: Image):
+    width, height = image.size
+    border = (
+            [image.getpixel((0, j)) for j in range(height)] +
+            [image.getpixel((width - 1, j)) for j in range(height)] +
+            [image.getpixel((i, 0)) for i in range(width)] +
+            [image.getpixel((i, height - 1)) for i in range(width)])
+    transparency = Counter(border).most_common(1)[0][0]
+    return transparency
+
+
 def check_image(image: Image, current_directory: str):
     global exe_directory
+    global total_sprites_unchanged
     global total_sprites_tmc
-    global total_sprites_wasteful
-    global total_sprites_transparent_top_row
+    global total_sprites_expanded
+    global total_sprites_cropped
 
-    allowed_colors = 15
+    allowed_colors = 0xf
     image_changed = False
     image_filename = image.filename
     palette_indexes = set(image.tobytes())
 
     # Check if the image has too many colors. If so, convert to 16-bit indexed.
     if max(palette_indexes) > allowed_colors:
-        log('Converted: ' + image_filename + ' had ' + str(max(palette_indexes)) +
-            ' colors, which is more than the maximum allowed (' + str(allowed_colors) + ').')
         if image.mode == "P":
             # Images already in P mode cannot be converted to P mode to shrink their allowed colors, so
             #   temporarily convert them back to RGB
-            image = image.convert("RGB")
+            image = image.convert("RGBA")
         image = image.convert("P", palette=Image.ADAPTIVE, colors=allowed_colors)
         image_changed = True
         total_sprites_tmc = total_sprites_tmc + 1
+        log('Reduced the number of colors to the maximum supported (' + str(allowed_colors) + '): ' + image_filename)
 
-    # Check if the image is wasting colors (why?)
-    # is_8color = max(palette_indexes) <= 7
-    # if len(palette_indexes) <= 8 and not is_8color and hasattr(image, 'filename'):
-    #    log('INFO: %s has a wasteful palette.' % image_filename)
-    #    total_sprites_wasteful = total_sprites_wasteful + 1
-
-    # Check if the top row of the image is transparent
-    transparent_top = True
-    transparent_bottom = True
-    transparent_left = True
-    transparent_right = True
+    # Check if the top row and left column of the image are excess border
+    image = image.convert('RGBA')
+    border_color = get_transparency(image)
+    solid_top = True
+    solid_left = True
     had_transparent_border = False
-    image_width_in_pixels, image_height_in_pixels = image.size
 
-    while transparent_top or transparent_bottom or transparent_left or transparent_right:
-        pixel_data = image.convert('RGBA')
+    image_width_in_pixels, image_height_in_pixels = image.size
+    while solid_top:
+        current_color = border_color
         for x in range(image_width_in_pixels):
-            # Scan the top and bottom rows of pixels, cropping out the row if it is fully transparent
-            current_alpha_1 = pixel_data.getpixel((x, 0))[3]
-            current_alpha_2 = pixel_data.getpixel((x, image_height_in_pixels - 1))[3]
-            if not current_alpha_1 == 0:
-                transparent_top = False
-            if not current_alpha_2 == 0:
-                transparent_bottom = False
-            if not transparent_top and not transparent_bottom:
+            # Scan the top row of pixels, cropping out the row if it is fully transparent
+            if not current_color == image.getpixel((x, 0)):
+                solid_top = False
+            if not solid_top:
                 break
-            if x == image_width_in_pixels - 1 and transparent_top:
+            if x == image_width_in_pixels - 1 and solid_top:
                 # If the top row was fully transparent, crop it out
+                had_transparent_border = True
                 image = image.crop((0, 1, image_width_in_pixels, image_height_in_pixels))
                 image_width_in_pixels, image_height_in_pixels = image.size
-                had_transparent_border = True
-            if x == image_width_in_pixels - 1 and transparent_bottom:
-                # If the bottom row was fully transparent, crop it out
-                image = image.crop((0, 0, image_width_in_pixels, image_height_in_pixels - 1))
-                image_width_in_pixels, image_height_in_pixels = image.size
-                had_transparent_border = True
 
+    while solid_left:
+        current_color = border_color
         for y in range(image_height_in_pixels):
-            # Scan the left and right columns of pixels, cropping out the column if it is fully transparent
-            current_alpha_3 = pixel_data.getpixel((0, y))[3]
-            current_alpha_4 = pixel_data.getpixel((image_width_in_pixels - 1, y))[3]
-            if current_alpha_3 and not current_alpha_3 == 0:
-                transparent_left = False
-            if current_alpha_4 and not current_alpha_4 == 0:
-                transparent_right = False
-            if not transparent_left and not transparent_right:
+            # Scan the left column of pixels, cropping out the column if it is fully transparent
+            if not current_color == image.getpixel((0, y)):
+                solid_left = False
+            if not solid_left:
                 break
-            if y == image_height_in_pixels - 1 and transparent_left:
+            if y == image_height_in_pixels - 1 and solid_left:
                 # If the left column was fully transparent, crop it out
+                had_transparent_border = True
                 image = image.crop((1, 0, image_width_in_pixels, image_height_in_pixels))
                 image_width_in_pixels, image_height_in_pixels = image.size
-                had_transparent_border = True
-            if y == image_height_in_pixels - 1 and transparent_right:
-                # If the right column was fully transparent, crop it out
-                image = image.crop((0, 0, image_width_in_pixels - 1, image_height_in_pixels))
-                image_width_in_pixels, image_height_in_pixels = image.size
-                had_transparent_border = True
+
     if had_transparent_border:
-        log('Cropped: ' + image_filename + ' had a transparent border row or column.')
+        log('Cropped extra border space from the top row and left column: ' + image_filename + '.')
         image_changed = True
-        total_sprites_transparent_top_row = total_sprites_transparent_top_row + 1
+        total_sprites_cropped = total_sprites_cropped + 1
+
+    # Tiles are 8x8, so we ensure the image's width and height are divisible by 8
+    image_width_in_pixels, image_height_in_pixels = image.size
+    image_expanded = False
+    if not image_width_in_pixels % 8 == 0:
+        image_changed = True
+        image_expanded = True
+        border_width = 8 - (image_width_in_pixels % 8)
+        image = ImageOps.expand(image, border=(0, 0, border_width, 0), fill=border_color)
+
+    if not image_height_in_pixels % 8 == 0:
+        image_changed = True
+        image_expanded = True
+        border_width = 8 - (image_height_in_pixels % 8)
+        image = ImageOps.expand(image, border=(0, 0, 0, border_width), fill=border_color)
+
+    if image_expanded:
+        total_sprites_expanded = total_sprites_expanded + 1
+        log('Expanded the right and bottom borders to fill an 8x8 tile: ' + image_filename)
 
     if image_changed:
         # Move the old file to a backup location
         image.filename = image_filename
+        image = image.convert("P", palette=Image.ADAPTIVE, colors=allowed_colors)
         backup_location = os.path.join(exe_directory,
                                        "backup",
                                        current_directory[len(exe_directory) + 1:],
@@ -154,6 +167,8 @@ def check_image(image: Image, current_directory: str):
         os.makedirs(os.path.dirname(backup_location), exist_ok=True)
         os.replace(src=image_filename, dst=backup_location)
         image.save(image_filename)
+    else:
+        total_sprites_unchanged = total_sprites_unchanged + 1
 
 
 def log(line):
@@ -168,9 +183,10 @@ if __name__ == '__main__':
     try:
         print('This program will look through ALL .png files in the programs directory and up to 4 '
               'sub-directories.')
-        print('If a .png file has more than 15 colors, it will be converted to use only 15 colors.')
-        print('If it has a transparent border row or column, it will be cropped.')
-        print('The original image is saved in a backup directory.')
+        print('If a sprite has more than 15 colors, it will be converted to use only 15 colors.')
+        print('If a sprite has a excess border space on the top or left, it will be cropped.')
+        print('If a sprite does not evenly fill an 8x8 tile, the bottom and/or right borders will be expanded.')
+        print('The original image is saved in a backup directory in the script directory.')
         run_script = input('Continue? Y/N:  ')
         if run_script.lower() == "y":
             main()
